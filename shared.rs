eb88c13b5fa5036bac3a1f16a208a31275145552
huiinyg-rusting <huiinyg@outlook.com>
2026-08-11 14:05:12 +0800

    set build-time open timestamp to past (2023-11-14) for burn/cancel testing

--- shared.rs
+++ shared.rs
@@ -1,7 +1,7 @@
 #[allow(dead_code)]
 pub const PASSWORD: &[u8] = b"114514";
 pub const KEY_LEN: usize = 64;
-pub const OPEN_TIMESTAMP_UNIX_SECONDS: u64 = 2_755_000_000;
+pub const OPEN_TIMESTAMP_UNIX_SECONDS: u64 = 1_700_000_000;
 pub const NTP_SERVERS: [&str; 5] = [
     "ntp.aliyun.com",
     "ntp.myhuaweicloud.com",
